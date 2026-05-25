b7cf6843474879350f3c0434472a01b7b3553d1d
avmarvel2008-star <avmarvel2008@gmail.com>
2026-05-25 17:33:55 +0530

    Connected MongoDB database

--- Backend/server.js
+++ Backend/server.js
@@ -1,14 +1,20 @@
 const express = require('express');
 const cors = require('cors');
+const mongoose = require('mongoose');
+require('dotenv').config();
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => console.log('MongoDB Connected!'))
+  .catch(err => console.log(err));
+
 app.get('/', (req, res) => {
   res.send('CMS Backend is running!');
 });
 
-app.listen(5000, () => {
+app.listen(process.env.PORT, () => {
   console.log('Server started on port 5000');
 });
