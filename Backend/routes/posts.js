77da47465b9f8e56e3f32b9c28d325a4da33bc7f
avmarvel2008-star <avmarvel2008@gmail.com>
2026-05-26 10:29:37 +0530

    Added Blog Post schema and routes

---
+++ Backend/routes/posts.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const router = express.Router();
+const Post = require('../models/Post');
+
+// GET all posts
+router.get('/', async (req, res) => {
+  try {
+    const posts = await Post.find();
+    res.json(posts);
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+});
+
+// CREATE a new post
+router.post('/', async (req, res) => {
+  try {
+    const post = new Post(req.body);
+    await post.save();
+    res.json(post);
+  } catch (err) {
+    res.status(400).json({ message: err.message });
+  }
+});
+
+module.exports = router;
--- Backend/server.js
+++ Backend/server.js
@@ -7,6 +7,9 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+const postRoutes = require('./routes/posts');
+app.use('/api/posts', postRoutes);
+
 mongoose.connect(process.env.MONGO_URI)
   .then(() => console.log('MongoDB Connected!'))
   .catch(err => console.log(err));
